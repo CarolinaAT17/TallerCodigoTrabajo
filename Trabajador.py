4b28885e6a54a242760a56d8120d0b732453abbe
CarolinaAT17 <103219728+CarolinaAT17@users.noreply.github.com>
2022-07-05 22:12:12 -0400

    Add files via upload

--- Trabajador.py
+++ Trabajador.py
@@ -1,6 +1,30 @@
 import os
 os.system("cls")
 
+
+class Trabajador:
+    def __init__(self, rut_f, nombre, apellido, direccion, telefono, fecha_ingreso):
+        self.rut_f = rut_f
+        self.nombre = nombre
+        self.apellido = apellido
+        self.direccion = direccion
+        self.telefono = telefono
+        self.fecha_ingreso = fecha_ingreso
+
+# AGREGAR
+def AgregarTrabajador():
+    rut_t = input("Ingresar rut: ")
+    nombre = input("Ingresar nombre: ")
+    apellido = input("Ingresar apellido: ")
+    direccion = input("Ingresar direccion: ")
+    telefono = input("Ingresar telefono: ")
+    fecha_ingreso = input("Ingresar fecha(dd-mm-yyyy): ")
+
+    a = Trabajador(rut_t, nombre, apellido, direccion, telefono, fecha_ingreso)
+
+    miCursor.execute("SELECT * FROM TRABAJADOR WHERE rut_t")
+
+
 def Trabajador():
     repeat = True
 
